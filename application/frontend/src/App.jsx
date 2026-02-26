1748d003a20373ac798fc49b290d0159d0f8f1e4
EmilianoMtzC <jemtzc.2007@gmail.com>
2026-02-26 12:53:25 -0600

    Correción Página Login

--- application/frontend/src/App.jsx
+++ application/frontend/src/App.jsx
@@ -1,17 +1,10 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-
-      <main style={{ marginTop: '100px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <h1>Vite + React</h1>
-        <p>jijiji</p>
-        
-      </main>
-    </div>
+    <Login/>
   )
 }
 
