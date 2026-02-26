f06a9c7afdbba8dfb6b5589063b335041d4bb8f1
EmilianoMtzC <jemtzc.2007@gmail.com>
2026-02-26 08:48:59 -0600

    Comp Boton y Org de proyecto

---
+++ application/frontend/src/Components/ButtonComponent.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function ButtonComponent({ text, onClick, api, type = "button", className = "", style }) {
+    const buttonStyle = {
+        backgroundColor: "#5D5D5D",
+        color: "white",
+        border: "none",
+        borderRadius: "20px",
+        padding: "6px 14px",
+        cursor: "pointer",
+        fontWeight: "500",
+        fontSize: "0.9rem",
+        ...style
+    };
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const handleClick = async (event) => {
+        console.log("equisde");
+    }
+
+    return (
+        <button
+            className={className}
+            style={buttonStyle}
+            onClick={handleClick}
+            disabled={loading}
+            type={type}
+        >
+            {loading ? "..." : text}
+            {error ? ` (${error})` : ""}
+        </button>
+    );
+
+}
+export default ButtonComponent;
